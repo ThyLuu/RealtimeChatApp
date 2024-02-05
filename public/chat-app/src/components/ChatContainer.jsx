6ca945ee63cfecd2186cc0bb28633fa0a8fdad28
LVAK-NapoleonII <124894637+LVAK-NapoleonII@users.noreply.github.com>
2024-02-05 23:16:51 +0700

    3/2

--- public/chat-app/src/components/ChatContainer.jsx
+++ public/chat-app/src/components/ChatContainer.jsx
@@ -1,8 +1,72 @@
-import React from 'react'
+import React,{useState, useEffect, useRef} from 'react'
 import LogOut from './LogOut'
+import ChatInput from './Chatinput';
+import axios from 'axios';
+import {v4 as uuidv4} from "uuid";
+import { getAllMessagesRoute, sendMessageRoute } from '../utils/APIRoutes';
+//xử lý tin nhắn
+export default function ChatContainer({currentChat,currentUser,socket}) {
 
+    const [messages, setMessages] = useState([]);
+    const [arrivalMessage,setArrivalMessage] = useState(null)
+    const scrollRef = useRef();
 
-export default function ChatContainer({currentChat}) {
+    const fetchMessages = async (msg) => {
+        console.log(currentChat)
+        console.log(currentUser);
+        if(currentChat){
+            const response = await axios.post(getAllMessagesRoute, {
+                from: currentUser._id,
+                to: currentChat._id,
+                message: msg,
+            });
+            setMessages(response.data);
+        }
+    }
+
+    useEffect(() => {
+        fetchMessages();
+    }, [currentUser,currentChat]);
+//    useEffect(async() => {
+//         const response = await axios.post(getAllMessagesRoute,{
+//             from: currentUser._id,
+//             to: currentChat._id,
+//         });
+//         setMessages(response.data);
+//     },[currentChat]);
+
+    const handleSendMsg = async (msg) => {
+        await axios.post(sendMessageRoute, {
+            from: currentUser._id,
+            to: currentChat._id,
+            message: msg,
+          });
+          socket.current.emit("send-msg",{
+            to: currentChat._id,
+            from:currentUser._id,
+            message:msg,
+          });
+          const msgs = [...messages];
+          msgs.push({fromSelf: true,message:msg});
+          setMessages(msgs);
+    } ;
+
+    useEffect (()=>{
+        if(socket.current){
+            socket.current.on("msg-recieve",(msg)=>{
+                setArrivalMessage({fromSelf:false,message:msg});
+            });
+        }
+    },[]);
+
+    useEffect(()=>{
+        arrivalMessage && setMessages((prev)=>[...prev,arrivalMessage]);
+    },[arrivalMessage])
+
+    useEffect(()=>{
+        scrollRef.current?.scrollIntoView({behavior:"smooth"});
+    },[messages]);
+
   return (
     <>
     {
@@ -21,9 +85,27 @@ export default function ChatContainer({currentChat}) {
         <LogOut/>
         </div>
 
-        <div className='chat-messages'></div>
+        { <div className='chat-messages flex gap-x-10 overflow-auto flex-col p-10'>
+            {
+                messages.map((message) => {
+                    return(
+                        <div ref={scrollRef} key={uuidv4()}>
+                            <div className={`message flex flex-col gap-1 overflow-auto ${message.fromSelf ? "sended justify-items-end" : "recieved"}`}>
+                                <div className='content max-5 border-r-2'>
+                                    <p>
+                                        {message.message}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                
+                }
+             )
+            }
+        </div> }
 
-        <div className='chat-input'></div>
+        <ChatInput handleSendMsg={handleSendMsg}></ChatInput>
         </div>
 
         )
@@ -33,4 +115,5 @@ export default function ChatContainer({currentChat}) {
     
     
   )
+
 }
---
+++ server/controllers/messagesController.js
@@ -0,0 +1,36 @@
+const MessageModel = require("../models/messageModel");
+
+// Thêm tin nhắn
+module.exports.addMessage = async (req, res, next) => {
+  try {
+    const { from, to, message } = req.body;
+    const data = await MessageModel.create({
+      message: { text: message },
+      users: [from, to],
+      sender: from,
+    });
+    if (data) return res.json({ msg: "Message added successfully." });
+    else return res.json({ msg: "Failed to add message to the database" });
+  } catch (ex) {
+    next(ex);
+  }
+};
+
+// Nhận tin nhắn
+module.exports.getAllMessage = async (req, res, next) => {
+  try {
+    const { from, to } = req.body;
+    const messages = await MessageModel.find({
+      users: { $all: [from, to], },
+    }).sort({ updatedAt: 1 });
+    const projectedMessages = messages.map((msg) => {
+      return {
+        fromSelf: msg.sender.toString() === from,
+        message: msg.message.text,
+      };
+    });
+    res.json(projectedMessages);
+  } catch (ex) {
+    next(ex);
+  }
+};
